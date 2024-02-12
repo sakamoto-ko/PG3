ba1e1402884ef5c244d2cd3a4f65e65eba766294
sakamoto-ko <115054832+sakamoto-ko@users.noreply.github.com>
2024-02-12 15:38:16 +0900

    a

--- main.cpp
+++ main.cpp
@@ -4,19 +4,26 @@
 #include <cstdlib>
 #include <ctime>
 #include <functional>
-
-#include "Circle.h"
-#include "Rectangle.h"
+#include <stdlib.h>
+#include <vector>
+#include <list>
+using namespace std;
 
 int main(int argc, const char* argv[]) {
-	Circle* circle = new Circle();
-	RectangleShape* rectangle = new RectangleShape();
 
-	circle->Size();
-	rectangle->Size();
+	list<const char*> lst{
+	"Tokyo","Kanda","Akihabara","Okachimachi","Ueno","Uguishudani","Nippori",
+	"Tabata","Komagome","Sugamo","Otsuka","Ikebukuro","Mejiro","Takadanobaba","Shin-Okubo",
+	"Shinjuku","Yoyogi","Harajuku","Shibuya","Ebisu","Meguro","Gotanta","Osaki","Shinagawa",
+	"Tamachi","Hamamatsucho","Shinbashi","Yurakucho"
+	};
 
-	circle->Draw();
-	rectangle->Draw();
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		if (*itr == "Nippori") {
+			itr = lst.insert("Nishi-Nippori", "Nippori");
+			++itr;
+		}
+	}
 
 	return 0;
 }
