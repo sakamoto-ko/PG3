161318be00c7b98d168312c5751c42df660deb9e
sakamoto-ko <115054832+sakamoto-ko@users.noreply.github.com>
2024-02-13 21:34:03 +0900

    a

--- main.cpp
+++ main.cpp
@@ -4,22 +4,16 @@
 #include <cstdlib>
 #include <ctime>
 #include <functional>
+#include <string>
 
 #include "MyMath.h"
 
 int main(int argc, const char* argv[]) {
-	MyMath<int, int> b1(10, 20);
-	MyMath<float, float> b2(10.0f, 20.0f);
-	MyMath<double, double> b3(10.0, 20.0);
-	MyMath<int, float> b4(10, 20.0f);
-	MyMath<int, double > b5(10, 20.0);
-	MyMath<float, double> b6(10.0f, 20.0);
+	std::string a = (100000, "a");
 
-	printf("<int, int> 大きいのは %d\n", b1.Min());
-	printf("<float, float> 大きいのは %f\n", b2.Min());
-	printf("<double, double> 大きいのは %lf\n", b3.Min());
-	printf("<int, float> 大きいのは %d\n", b4.Min());
-	printf("<int, double> 大きいのは %d\n", b5.Min());
-	printf("<float, double> 大きいのは %f\n", b6.Min());
+	for (auto i = a.begin(); i < a.end(); ++i) {
+		printf("%c", a[i]);
+	}
+
 	return 0;
 }
