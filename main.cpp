a393b70a9f43a919914500713a7f4eb16947eb17
sakamoto-ko <115054832+sakamoto-ko@users.noreply.github.com>
2024-02-13 22:00:57 +0900

    a

--- main.cpp
+++ main.cpp
@@ -5,15 +5,36 @@
 #include <ctime>
 #include <functional>
 #include <string>
+#include <chrono>
+#include <utility>
 
-#include "MyMath.h"
+using namespace std;
 
-int main(int argc, const char* argv[]) {
-	std::string a = (100000, "a");
+//#include "MyMath.h"
 
-	for (auto i = a.begin(); i < a.end(); ++i) {
-		printf("%c", a[i]);
-	}
+int main() {
+	std::string a(100000, 'a');
+
+	printf("100,000文字を移動とコピーで比較しました。\n");
+
+	std::chrono::system_clock::time_point  start1, end1; 
+	start1 = std::chrono::system_clock::now(); // 計測開始時間
+
+	std::string b = a;
+
+	end1 = std::chrono::system_clock::now();  // 計測終了時間
+	double elapsed1 = std::chrono::duration_cast<std::chrono::microseconds>(end1 - start1).count(); //処理に要した時間をミリ秒に変換
+
+	std::chrono::system_clock::time_point  start2, end2; 
+	start2 = std::chrono::system_clock::now(); // 計測開始時間
+
+	std::string c = std::move(a);
+
+	end2 = std::chrono::system_clock::now();  // 計測終了時間
+	double elapsed2 = std::chrono::duration_cast<std::chrono::microseconds>(end2 - start2).count(); //処理に要した時間をミリ秒に変換
+
+	printf("コピー: %lfμs\n", elapsed1);
+	printf("移動: %lfμs\n", elapsed1);
 
 	return 0;
 }
